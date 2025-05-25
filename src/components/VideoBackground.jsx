41749141bd181fa23055601ab6c5a16df9477b43
Abhilasha Choudhary <choudharyac02@gmail.com>
2025-05-25 21:51:18 +0530

    refactor: abstract logic in custom hook for fetching movies clips and update store with trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,28 +1,8 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/moviesSlice";
+
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
-  const dispatch = useDispatch();
-  const movieTrailer = useSelector((store) => store.movies?.trailerVideo);
-  const trailerKey = movieTrailer?.key;
-  console.log("trailer key", trailerKey);
-  const getMovieVideos = async () => {
-    const clips = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
-      API_OPTIONS
-    );
-    const data = await clips.json();
-
-    const filterData = data.results.filter((clip) => clip.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : data.results[0];
-    dispatch(addTrailerVideo(trailer));
-    console.log("clips", typeof yt_key, trailer);
-  };
-  useEffect(() => {
-    getMovieVideos();
-  }, []);
+ const trailerKey=useMovieTrailer(movieId);
   return (
     <div className="w-screen">
       <iframe
---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { addTrailerVideo } from "../utils/moviesSlice";
+import { useEffect } from "react";
+
+//fetch movies clip using movieId and update store with trailer key
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+  const movieTrailer = useSelector((store) => store.movies?.trailerVideo);
+  const trailerKey = movieTrailer?.key;
+  console.log("trailer key", trailerKey);
+  const getMovieVideos = async () => {
+    const clips = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos`,
+      API_OPTIONS
+    );
+    const data = await clips.json();
+
+    const filterData = data.results.filter((clip) => clip.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : data.results[0];
+    dispatch(addTrailerVideo(trailer));
+    console.log("clips", typeof yt_key, trailer);
+  };
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+  return trailerKey;
+};
+export default useMovieTrailer;
